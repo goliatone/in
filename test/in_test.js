57d6f49c5a90ef0923f7e7a0734060e65bd77e47
goliatone <burgosemi@gmail.com>
2016-10-23 17:45:02 -0400

    wip tests

--- test/in_test.js
+++ test/in_test.js
@@ -2,13 +2,11 @@
 
 var Plugin = require('../index');
 
-var assert = require('chai').assert;
 var fs = require('fs');
+var sinon = require('sinon');
 var path = require('path');
-var exec = require('child_process').exec;
+var assert = require('chai').assert;
 var fixture = path.resolve.bind(path, __dirname, 'fixtures');
-
-
 
 describe('in: Plugin loader', function(){
 
@@ -22,23 +20,6 @@ describe('in: Plugin loader', function(){
             assert.isFunction(Plugin.normalize);
         });
 
-        it('should expose a "sort" function', function(){
-            assert.isFunction(Plugin.sort);
-        });
-
-        it('should expose a "handler" function', function(){
-            assert.isFunction(Plugin.handler);
-        });
-
-        it('should expose a "mount" function', function(){
-            assert.isFunction(Plugin.mount);
-        });
-
-        it('should have PWD as default root value', function(){
-            assert.ok(Plugin.DEFAULTS.root, process.env.PWD)
-        });
-
-
         describe('∆ normalize', function(){
             it('should return an array if given an array', function(){
                 assert.ok(Plugin.normalize([]), []);
@@ -48,18 +29,27 @@ describe('in: Plugin loader', function(){
                 assert.ok(Plugin.normalize({}), []);
             });
 
-            it('should return an array from object keys', function(){
-                var plugins = {
-                    './plugins/noop':{},
-                    './plugins/noop2':{}
-                };
+            it('should return a normalized array from a String', function(){
+                var plugins = '/Users/application/plugins/authentication';
+                var expected = [
+                    {
+                        "id": "authentication",
+                        "path": "/Users/application/plugins/authentication",
+                        "config": {}
+                    }
+                ];
+
+                assert.deepEqual(Plugin.normalize(plugins), expected);
+            });
+
+            it('should normalize an array of paths', function(){
+                var plugins = ['/Users/application/plugins/authentication'];
 
                 var expected = [
                     {
-                        './plugins/noop':{},
-                    },
-                    {
-                        './plugins/noop2':{}
+                        "id": "authentication",
+                        "path": "/Users/application/plugins/authentication",
+                        "config": {}
                     }
                 ];
 
@@ -68,31 +58,15 @@ describe('in: Plugin loader', function(){
 
             it('should return an array from complex object keys', function(){
                 var plugins = {
-                    './plugins/noop': {
-                        config:{
-                            a: true
-                        }
-                    },
-                    './plugins/noop2':{
-                        config:{
-                            a: true
-                        }
-                    }
+                    '/Users/application/plugins/authentication': { hash: 'sh1' }
                 };
 
                 var expected = [
                     {
-                        './plugins/noop': {
-                            config:{
-                                a: true
-                            }
-                        },
-                    },
-                    {
-                        './plugins/noop2':{
-                            config:{
-                                a: true
-                            }
+                        "id": "authentication",
+                        "path": "/Users/application/plugins/authentication",
+                        "config": {
+                            "hash": "sh1"
                         }
                     }
                 ];
@@ -101,24 +75,135 @@ describe('in: Plugin loader', function(){
             });
         });
 
-        describe('∆ handler', function(){
-            it('default handler should return what it was fed', function(){
-                var expected = {};
-                assert.equal(Plugin.handler(expected), expected);
+        describe('∆ mountHandler', function(){
+            it('default mountHandler should call bean.init', function(){
+
+                var bean = {
+                    plugin: {
+                        init: function(context, config){}
+                    }
+                };
+                var config = {};
+                var context = {};
+
+                var spy = sinon.spy(bean.plugin, 'init');
+
+                Plugin.DEFAULTS.mountHandler(bean, context, config);
+
+                assert.ok(spy.calledOnce);
+                assert.ok(spy.calledWith(config, context));
             });
 
+            it('default mountHandler should call bean.config if its a function', function(){
+
+                var bean = {
+                    config: function(context, config){},
+                    plugin: {}
+                };
+                var config = {};
+                var context = {};
+
+                var spy = sinon.spy(bean, 'config');
+
+                Plugin.DEFAULTS.mountHandler(bean, context, config);
+
+                assert.ok(spy.calledOnce);
+                assert.ok(spy.calledWith(bean.plugin, config, context));
+            });
+
+            it('default mountHandler should call bean.config.mount if its a function', function(){
+
+                var bean = {
+                    config: {
+                        mount: function(context, config){}
+                    },
+                    plugin: {}
+                };
+                var config = {};
+                var context = {};
+
+                var spy = sinon.spy(bean.config, 'mount');
+
+                Plugin.DEFAULTS.mountHandler(bean, context, config);
+
+                assert.ok(spy.calledOnce);
+            });
+
+            it('default mountHandler should add a property in context with the plugin', function(){
+
+                var bean = {
+                    id: 'plugin',
+                    plugin: {}
+                };
+                var config = {};
+                var context = {};
+
+                Plugin.DEFAULTS.mountHandler(bean, context, config);
+
+                assert.ok(context.plugin === bean.plugin);
+            });
         });
 
-        xit('should load plugins', function(done){
-            var options = {
-                root: process.env.PWD,
-                handler: function(plugin){
-                    assert.ok(plugin);
-                    done();
-                }
-            };
-            var plugins = fixture('local/local.json');
-            Plugin(plugins, options);
+        describe('∆ sortFilter', function(){
+            it('default sortFilter should sort based on negative priority', function(){
+                var plugins = [
+                    {id: 'a', priority: -100},
+                    {id: 'c', priority: 100},
+                    {id: 'b'},
+                ];
+                var expected = [
+                    {id: 'a', priority: -100},
+                    {id: 'b' },
+                    {id: 'c', priority: 100},
+                ];
+
+                plugins.sort(Plugin.DEFAULTS.sortFilter);
+                assert.deepEqual(plugins, expected);
+            });
+        });
+
+        describe('∆ find', function(){
+            it('should list plugins in a given directory', function(done){
+                var loader = new Plugin({
+                    basepath: __dirname
+                });
+
+                var expected = [
+                    'authentication',
+                    'logger.js',
+                    'pubsub.js',
+                    'repl.js'
+                ];
+
+                loader.find('./fixtures/mountDirectory/plugins').then((plugins)=>{
+                    plugins = plugins.map((plugin) => require('path').basename(plugin));
+                    assert.deepEqual(expected, plugins);
+                    done()
+                }).catch(done);
+            });
+        });
+
+        describe('∆ filter', function(){
+            it('should list plugins in a given directory', function(done){
+                var loader = new Plugin({
+                    basepath: __dirname
+                });
+                var paths = [
+                    'authentication',
+                    'logger.js',
+                    'pubsub.js',
+                    'repl.js'
+                ];
+
+                var expected = [
+                    'authentication',
+                ];
+
+                loader.filter(paths, ['**', '!*.js']).then((plugins)=>{
+                    assert.deepEqual(expected, plugins);
+                    done()
+                }).catch(done);
+            });
         });
     });
 });
