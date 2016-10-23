709f9e215777add9d2939dd48ef58e3145ffe660
goliatone <burgosemi@gmail.com>
2016-10-23 15:03:05 -0400

    wip

--- examples/normalize.js
+++ examples/normalize.js
@@ -2,24 +2,24 @@
 var normalize = require('..').normalize;
 
 var paths = [
-    '/Users/gol/plugins/authentication',
-    '/Users/gol/plugins/logger.js',
-    '/Users/gol/plugins/pubsub.js',
+    '/Users/application/plugins/authentication',
+    '/Users/application/plugins/logger.js',
+    '/Users/application/plugins/pubsub.js',
     'debug'
 ];
 
 
 var mixed = [
-    '/Users/gol/plugins/authentication',
-    '/Users/gol/plugins/logger.js',
-    {'/Users/gol/plugins/pubsub.js':{endpoing: 'URL', admin:'admin', pass:'pass'}},
+    '/Users/application/plugins/authentication',
+    '/Users/application/plugins/logger.js',
+    {'/Users/application/plugins/pubsub.js':{endpoing: 'URL', admin:'admin', pass:'pass'}},
     'debug'
 ];
 
 var obj = {
-    '/Users/gol/plugins/authentication':{hash:'sh1'},
-    '/Users/gol/plugins/logger.js': {level:'info'},
-    '/Users/gol/plugins/pubsub.js': {endpoing: 'URL', admin:'admin', pass:'pass'},
+    '/Users/application/plugins/authentication':{hash:'sh1'},
+    '/Users/application/plugins/logger.js': {level:'info'},
+    '/Users/application/plugins/pubsub.js': {endpoing: 'URL', admin:'admin', pass:'pass'},
     'debug':{}
 };
 
