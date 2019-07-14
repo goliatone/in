4eecb0848f5b33d5146d8d3bc65c5efbbf343df7
goliatone <burgosemi@gmail.com>
2019-07-13 17:31:50 -0700

    fix test

--- test/fixtures/mountDirectory/plugins/pubsub.js
+++ test/fixtures/mountDirectory/plugins/pubsub.js
@@ -1,11 +1,12 @@
 'use strict';
 
-module.exports.init = function(app, config){
+module.exports.init = function(app, config) {
+
     app.pubsub = {
-        publibsh: function(type, event){
+        publibsh: function(type, event) {
             app.logger.warn('publish: %s. payload: %s', type, event);
         },
-        subscribe: function(type, handler){
+        subscribe: function(type, handler) {
             app.logger.warn('subscribe to %s', type);
         }
     };
